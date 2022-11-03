e02e6114fca086de93317019d2c4ac21e33b469b
Irawan Rofi <irawan.rofi.ft21@mail.umy.ac.id>
2022-11-03 16:10:35 +0700

    Add void input

--- Program.cs
+++ Program.cs
@@ -13,5 +13,28 @@ namespace Exercise2
         private int mov_count = 0;
         private int n;
 
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Masukkan Angka Di Element Array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray mempunyai maskimal 20 element \n");
+            }
+            Console.WriteLine("\n=================");
+            Console.WriteLine("Enter Array Element");
+            Console.WriteLine("\n=================");
+
+            for (int i = 0; i < n; i ++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
     }
 }
