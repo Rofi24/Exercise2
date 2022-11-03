2781ae674f0e3166492d3ee175d2820299fa30a6
Irawan Rofi <irawan.rofi.ft21@mail.umy.ac.id>
2022-11-03 16:03:34 +0700

    Add class Progran

--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@ namespace Exercise2
 {
     class Program
     {
+        private int[] arr = new int[20];
+        private int cmp_count = 0;
+        private int mov_count = 0;
+        private int n;
 
-        static void Main(string[] args)
-        {
-        }
     }
 }
